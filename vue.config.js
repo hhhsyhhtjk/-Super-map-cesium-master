02bd430ab15a8d62b6e125d2fd2ca42747ddedd7
hhhsyhhtjk <hhhsyhhtjk@users.noreply.github.com>
2026-09-18 20:24:43 +0800

    Deploy SuperMap Cesium reservoir demo

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV === 'production' ? '/-Super-map-cesium-master/' : '/',
+    transpileDependencies: true,
+    lintOnSave: false,
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = '数字孪生富水水库';
+            return args;
+        });
+    },
+    devServer: {
+        //http://10.42.173.20:8088
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:8066/', // 目标域
+        //         changeOrigin: true, // 允许跨域
+        //         pathRewrite: {
+        //             '^/api': '' // 重写路径
+        //         }
+        //     }
+        // }
+    }
+});
